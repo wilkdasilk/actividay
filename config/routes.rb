c34e8487a89aa4832ee29a0ad47d96935518a3ce
wilkdasilk <anwilk92@gmail.com>
2017-03-24 14:59:19 -0700

    user accepts all fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "splash#index"
 
+  get "/users", to: "users#index", as: "users"
+  get "/users/:id", to: "users#show", as: "user"
 end

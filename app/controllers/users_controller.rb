29086f9aa66e8d70d8a77b174e8c0edbbf9f234c
Logan Medina <loganjmedina@gmail.com>
2017-03-25 10:59:10 -0700

    added a profile path, separate from the user path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def profile
+    if current_user
+      @user = current_user
+    else
+      redirect_to splash_path
+    end
+  end
+
   def show
     user_id = params[:id]
     @user = User.find_by_id(user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # Why custom registrations? To handle additional user fields: http://jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "splash#index"
+  root to: "users#profile"
+
+  get "/welcome", to: "splash#index", as: "splash"
 
   #user read routes
   get "/users", to: "users#index", as: "users"

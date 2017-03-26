fab5e8b162d9f7ca86a6f5891128df6725a9a8a5
Logan Medina <loganjmedina@gmail.com>
2017-03-25 21:42:44 -0700

    ran my migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325030924) do
+ActiveRecord::Schema.define(version: 20170326043820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20170325030924) do
     t.integer  "status",      default: 0
     t.index ["activity_id"], name: "index_challenges_on_activity_id", using: :btree
     t.index ["user_id"], name: "index_challenges_on_user_id", using: :btree
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.integer  "challenge_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["challenge_id"], name: "index_posts_on_challenge_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +63,5 @@ ActiveRecord::Schema.define(version: 20170325030924) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "challenges"
 end

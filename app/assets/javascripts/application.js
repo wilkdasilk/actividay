e6fc0efef274adba90b80ea2c1b7e199d75244dd
wilkdasilk <anwilk92@gmail.com>
2017-03-27 15:01:30 -0700

    front end upload validation fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,32 +26,30 @@ $(document).on('turbolinks:load', function(){
 
    // called on file upload, client-side
    // source: http://stevenyue.com/blogs/validate-attachment-file-size-and-type-in-rails/
-   $(document).on('turbolinks:load', function(){
-     function validateFiles(inputFile) {
-      var maxExceededMessage = "This file exceeds the maximum allowed file size (5 MB)";
-      var extErrorMessage = "Only image file with extension: .jpg, .jpeg, .gif or .png is allowed";
-      var allowedExtension = ["jpg", "jpeg", "gif", "png"];
+   function validateFiles(inputFile) {
+    var maxExceededMessage = "This file exceeds the maximum allowed file size (5 MB)";
+    var extErrorMessage = "Only image file with extension: .jpg, .jpeg, .gif or .png is allowed";
+    var allowedExtension = ["jpg", "jpeg", "gif", "png"];
 
-      var extName;
-      var maxFileSize = $(inputFile).data('max-file-size');
-      var sizeExceeded = false;
-      var extError = false;
+    var extName;
+    var maxFileSize = $(inputFile).data('max-file-size');
+    var sizeExceeded = false;
+    var extError = false;
 
-      $.each(inputFile.files, function() {
-        if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};
-        extName = this.name.split('.').pop();
-        if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
-      });
-      if (sizeExceeded) {
-        window.alert(maxExceededMessage);
-        $(inputFile).val('');
-      };
+    $.each(inputFile.files, function() {
+      if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};
+      extName = this.name.split('.').pop();
+      if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
+    });
+    if (sizeExceeded) {
+      window.alert(maxExceededMessage);
+      $(inputFile).val('');
+    };
 
-      if (extError) {
-        window.alert(extErrorMessage);
-        $(inputFile).val('');
-      };
-     }
+    if (extError) {
+      window.alert(extErrorMessage);
+      $(inputFile).val('');
+    };
    }
 
 });

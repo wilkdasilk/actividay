1227431623cf077ff74dbda687fbe2041cb3c950
wilkdasilk <anwilk92@gmail.com>
2017-03-26 19:44:46 -0700

    confine avatar dimensions

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,8 +1,10 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  process resize_to_fill: [256, 256]
+
 
   # Choose what kind of storage to use for this uploader:
   storage :file

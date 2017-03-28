8cb2c05fbb50c1348d3356b5258fe41e1aa4324e
Logan Medina <loganjmedina@gmail.com>
2017-03-28 12:23:14 -0700

    testing the mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,21 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'actividay.herokuapp.com' }
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :domain               => "actividay.herokuapp.com",
+   :authentication       => "plain",
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+  :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

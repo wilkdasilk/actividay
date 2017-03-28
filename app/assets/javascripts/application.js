60db10323d6c4879f0b94c814ad205183a8dde63
medinal <loganjmedina@gmail.com>
2017-03-27 21:05:12 -0700

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,18 +37,18 @@ $(document).on('turbolinks:load', function(){
     var extError = false;
 
     $.each(inputFile.files, function() {		
- -     if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};		
- -      extName = this.name.split('.').pop();		
- -     if ($.inArray(extName, allowedExtension) == -1) {extError=true;};		
- -   });		
- -   if (sizeExceeded) {		
- -     window.alert(maxExceededMessage);		
- -     $(inputFile).val('');		
- -   };		
- -		
- -   if (extError) {		
- -     window.alert(extErrorMessage);		
- -     $(inputFile).val('');		
- -   };		
- - }
+      if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};		
+       extName = this.name.split('.').pop();		
+      if ($.inArray(extName, allowedExtension) == -1) {extError=true;};		
+    });		
+    if (sizeExceeded) {		
+      window.alert(maxExceededMessage);		
+      $(inputFile).val('');		
+    };		
+ 		
+    if (extError) {		
+      window.alert(extErrorMessage);		
+      $(inputFile).val('');		
+    };		
+  }
 });

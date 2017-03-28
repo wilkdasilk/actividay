f373c55df26a6cec945724e6f74e71616bbec555
Logan Medina <loganjmedina@gmail.com>
2017-03-28 11:12:11 -0700

    fixed the profile to remove uninterested challenges and fixed the controller to not select activities that have been completed in the last 7 days

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,19 +36,19 @@ $(document).on('turbolinks:load', function(){
     var sizeExceeded = false;
     var extError = false;
 
-    $.each(inputFile.files, function() {		
-      if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};		
-       extName = this.name.split('.').pop();		
-      if ($.inArray(extName, allowedExtension) == -1) {extError=true;};		
-    });		
-    if (sizeExceeded) {		
-      window.alert(maxExceededMessage);		
-      $(inputFile).val('');		
-    };		
- 		
-    if (extError) {		
-      window.alert(extErrorMessage);		
-      $(inputFile).val('');		
-    };		
+    $.each(inputFile.files, function() {
+      if (this.size && maxFileSize && this.size > parseInt(maxFileSize)) {sizeExceeded=true;};
+       extName = this.name.split('.').pop();
+      if ($.inArray(extName, allowedExtension) == -1) {extError=true;};
+    });
+    if (sizeExceeded) {
+      window.alert(maxExceededMessage);
+      $(inputFile).val('');
+    };
+
+    if (extError) {
+      window.alert(extErrorMessage);
+      $(inputFile).val('');
+    };
   }
 });

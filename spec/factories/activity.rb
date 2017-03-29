6ff194c8d2bdf189bd0ea239734faf1e6ba31888
Logan Medina <loganjmedina@gmail.com>
2017-03-29 11:02:02 -0700

    setting up factories

--- spec/factories/activity.rb
+++ spec/factories/activity.rb
@@ -1,11 +1,5 @@
 FactoryGirl.define do
-  sequence :email do |n|
-    "person#{n}@example.com"
-  end
-end
 
-FactoryGirl.define do
-  
   factory :user, :class => 'User' do
     name 'test'
     born_on DateTime.new(2000, 1, 1)
